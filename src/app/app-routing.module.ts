4248359dea01ba78ae95d1a1a6d35f2d0540bada
SelimMeske <mesic.selim@outlook.com>
2020-08-27 17:25:49 +0200

    seo implementation

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -7,13 +7,41 @@ import { SuppStackComponent } from './frontend/calculation/supp-stack/supp-stack
 
 
 const routes: Routes = [
-  {path: '', component: HomeComponent},
-  {path: 'bmr', component: MainCalculationsComponent},
-  {path: 'tdee', component: MainCalculationsComponent},
-  {path: 'caloric-needs', component: MainCalculationsComponent},
-  {path: 'ideal-weight', component: MainCalculationsComponent},
-  {path: 'body-type', component: BodyTypeComponent},
-  {path: 'supplement-stack', component: SuppStackComponent}
+  {path: '', component: HomeComponent, data: {
+    title: 'Fitness Calculator | InstaFitCalculator.com',
+    description: 'Every fitness calculator that you need in one place.',
+    ogUrl: 'instafitcalculator.com'
+  }},
+  {path: 'bmr', component: MainCalculationsComponent, data: {
+    title: 'Calculate your basal metabolic rate | InstaFitCalculator.com',
+    description: 'Calculate the basal metabolic rate with InstaFitCalculator.',
+    ogUrl: 'instafitcalculator.com/bmr'
+  }},
+  {path: 'tdee', component: MainCalculationsComponent, data: {
+    title: 'Calories Burned Calculator | InstaFitCalculator.com',
+    description: 'You wonder how much calories do you burn? Find out now with InstaFitCalculator.',
+    ogUrl: 'instafitcalculator.com/tdee'
+  }},
+  {path: 'caloric-needs', component: MainCalculationsComponent, data: {
+    title: 'Caloric needs calculator | InstaFitCalculator.com',
+    description: 'You wonder how much calories do you need each day? Find out now with InstaFitCalculator.',
+    ogUrl: 'instafitcalculator.com/caloric-needs'
+  }},
+  {path: 'ideal-weight', component: MainCalculationsComponent, data: {
+    title: 'Calculate your ideal weight | InstaFitCalculator.com',
+    description: 'You wonder what is your ideal weight? Find out now with InstaFitCalculator.',
+    ogUrl: 'instafitcalculator.com/ideal-weight'
+  }},
+  {path: 'body-type', component: BodyTypeComponent, data: {
+    title: 'What is your body type? | InstaFitCalculator.com',
+    description: 'You wonder what is your body type? Find out now with InstaFitCalculator.',
+    ogUrl: 'instafitcalculator.com/body-type'
+  }},
+  {path: 'supplement-stack', component: SuppStackComponent, data: {
+    title: 'What is my ideal supplement stack? | InstaFitCalculator.com',
+    description: 'Find out what is your ideal supplement stack with InstaFitCalculator.',
+    ogUrl: 'instafitcalculator.com/supplement-stack'
+  }}
 ];
 
 @NgModule({
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit, OnDestroy } from '@angular/core';
-import { Router, NavigationEnd } from '@angular/router';
-import { Subscription } from 'rxjs';
+import { Router, NavigationEnd, ActivatedRoute } from '@angular/router';
+import { SEOService } from './services/seo.service';
+import { filter, map, mergeMap } from 'rxjs/operators';
+
 
 @Component({
   selector: 'app-root',
@@ -9,9 +11,10 @@ import { Subscription } from 'rxjs';
 })
 export class AppComponent implements OnInit, OnDestroy {
   
-  constructor(public router: Router) {}
+  constructor(public router: Router, private seoService: SEOService, private activatedRoute: ActivatedRoute) {}
   
   ngOnInit(){
+
     this.router.events.subscribe(e => {
       if(e instanceof NavigationEnd){
         (<any>window).ga('set', 'page', e.urlAfterRedirects);
@@ -19,6 +22,25 @@ export class AppComponent implements OnInit, OnDestroy {
         window.scroll(0, 0);
       }
     })
+
+    this.router.events.pipe(
+      filter(e => e instanceof NavigationEnd),
+      map(() => this.activatedRoute),
+      map((route) => {
+        while (route.firstChild) route = route.firstChild;
+        return route;
+      }),
+      filter((route) => route.outlet === 'primary'),
+      mergeMap((route) => route.data)
+    ).subscribe(e => {
+
+      let title: string = e['title'];
+      let description: string = e['description'];
+      let ogUrl: string = e['ogUrl'];
+      this.seoService.updateTitle(title);
+      this.seoService.updateDescription(description);
+      this.seoService.updateOgUrl(ogUrl);
+    });
   }
 
   ngOnDestroy(){
